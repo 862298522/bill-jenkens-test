b67baadcfb19c5bdef1d120693493275c38aa311
huangdalin <8622@98522@qq.com>
2023-12-18 14:29:35 +0800

    首次提交--

--- src/main/java/com/jenkens/controller/JenkensController.java
+++ src/main/java/com/jenkens/controller/JenkensController.java
@@ -1,9 +1,18 @@
 package com.jenkens.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author huangdalin
  * @Description TODO
  * Date 2023/12/18 10:25
  */
+
+@RestController
+@RequestMapping("/hello")
 public class JenkensController {
+    public String hello(){
+        return "Hello nice!";
+    }
 }
